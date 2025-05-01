43b05dd23e596c75322551097dab992c732a2c68
Rodrigo <rfnobando@gmail.com>
2025-05-01 20:06:31 -0300

    Add some basic rows in statuses and ticket_categories

---
+++ sql/02_init_statuses.sql
@@ -0,0 +1,5 @@
+INSERT INTO `hibernate_tickets`.`statuses` (`name`) VALUES
+('open'),
+('in_progress'),
+('resolved'),
+('closed');
---
+++ sql/03_init_ticket_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO `hibernate_tickets`.`ticket_categories` (`name`) VALUES
+('general_inquiries'),
+('customer_support'),
+('technical_support'),
+('bugs_and_errors'),
+('service_requests'),
+('maintenance'),
+('feedback');
